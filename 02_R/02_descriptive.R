e58cd9c89ca0a6a1bcf0ee00c3bce15922bd0ed6
L.P. Rojas Saunero <l.rojassaunero@erasmusmc.nl>
2021-01-28 14:28:09 +0100

    table one

--- 02_R/02_descriptive.R
+++ 02_R/02_descriptive.R
@@ -1,3 +1,9 @@
+mytablestyle <- function(df){
+  kable(df) %>% 
+    kable_styling(bootstrap_options = c("hover", "condensed", "responsive"),
+                  full_width = F,
+                  font_size = 14)}
+
 ## Descriptive analysis
 
 data <- import(here::here("01_data", "wide_noltfu.RData"))
@@ -23,16 +29,14 @@ data %>% group_by(dementia) %>%
 
 ### Table 1
 data_table1 <- data %>%
-  select(age_0, sex, education, apoe4, smoke1, sbp1, ht1, bmi1, mmse1, 
+  select(age_0, sex, education, apoe4, smoke_dic, sbp1, ht1, bmi1, mmse1, 
          stroke_prev, hd_prev, diabetes_prev, cancer_prev, cancer_inc, hd_inc,
          stroke_inc) %>% 
   mutate(education = str_to_title(str_replace_all(education, "_", " ")),
-         sex = ifelse(sex == 1, "Women", "Men"),
-         smoke1 = str_to_title(smoke1)) %>%
+         sex = ifelse(sex == 1, "Women", "Men")) %>%
   rename(Age = age_0,
          Education = education,
          Sex = sex,
-         Smoke = smoke1,
          Hypertension = ht1,
          `Systolic Blood Pressure (mmHg)` = sbp1,
          `Body Mass Index` = bmi1)
@@ -42,6 +46,22 @@ library(tableone)
 colnames(data_table1)
 categorical <- colnames(data_table1)[c(2:5, 7, 10:17)]
 
-print(CreateTableOne(data = data_table1, factorVars = categorical))
+table <-
+  print(CreateTableOne(
+    data = data_table1,
+    factorVars = categorical,
+    strata = "smoke_dic", test = FALSE
+  ))
+
+table_print <-
+  print(
+    table,
+    showAllLevels = TRUE,
+    quote = FALSE,
+    noSpaces = TRUE,
+    printToggle = FALSE
+  )
+
+mytablestyle(table_print)
 
 #### make counts of incident things
