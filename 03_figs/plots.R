825fe6bc3d022c7c8c35ff97d2b7543799e636c9
Paloma <palolili@gmail.com>
2021-02-02 16:30:30 +0100

    update plots

--- 03_figs/plots.R
+++ 03_figs/plots.R
@@ -1,50 +1,67 @@
 ####
 
-bind_all_models <- function(cif, death, km){
-  
-  tidy_cif <- cif %>% 
-  broom::tidy() %>% 
-  filter(state == "1") %>%
-  select(time, strata, estimate, conf.high, conf.low) %>%
-  rename(cif = estimate) %>% 
-  mutate(strata = ifelse(strata == "smoke_dic=0", "Never smoked", "Ever smoked")) %>% 
-  filter(
-    (strata == "Never smoked" & time %in% c(13, seq(24, 240, 12)))|
-      (strata == "Ever smoked" & time %in% c(seq(12, 240, 12)))) %>% 
-  arrange(strata, time) %>% 
-  group_by(strata) %>% 
-  mutate(time = row_number(),
-         model = "A. Total effect on dementia risk") %>% ungroup()
-  
-  tidy_death <- death %>% 
-    broom::tidy() %>% 
+bind_all_models <- function(cif, death, km) {
+  tidy_cif <- cif %>%
+    broom::tidy() %>%
     filter(state == "1") %>%
     select(time, strata, estimate, conf.high, conf.low) %>%
-    rename(cif = estimate) %>% 
-    mutate(strata = ifelse(strata == "smoke_dic=0", "Never smoked", "Ever smoked")) %>% 
-    filter(
-      (strata == "Never smoked" & time %in% c(13, seq(24, 240, 12)))|
-        (strata == "Ever smoked" & time %in% c(seq(12, 240, 12)))) %>% 
-    arrange(strata, time) %>% 
+    rename(cif = estimate) 
+  
+  time_zero <- tidy_cif %>% 
     group_by(strata) %>% 
-    mutate(time = row_number(),
+    slice(5) %>% 
+    ungroup() %>% 
+    mutate(across(where(is.numeric), ~.*0))
+  
+  tidy_cif %<>%
+    bind_rows(time_zero) %>% 
+    mutate(strata = ifelse(strata == "smoke_dic=0", 
+                           "Non-smokers", "Smokers")) %>%
+    filter((strata == "Non-smokers" &
+              time %in% c(seq(0, 240, 12))) |
+             (strata == "Smokers" &
+                time %in% c(seq(0, 240, 12)))) %>%
+    arrange(strata, time) %>%
+    group_by(strata) %>%
+    mutate(time = row_number() - 1,
+           model = "A. Total effect on dementia risk") %>% ungroup()
+
+tidy_death <- death %>%
+    broom::tidy() %>%
+    filter(state == "1") %>%
+    select(time, strata, estimate, conf.high, conf.low) %>%
+    rename(cif = estimate) %>%
+    bind_rows(time_zero) %>% 
+    mutate(strata = ifelse(strata == "smoke_dic=0",
+                           "Non-smokers", "Smokers")) %>%
+    filter((strata == "Non-smokers" &
+              time %in% c(seq(0, 240, 12))) |
+             (strata == "Smokers" &
+                time %in% c(seq(0, 240, 12)))) %>%
+    arrange(strata, time) %>%
+    group_by(strata) %>%
+    mutate(time = row_number() - 1,
            model = "C. Effect on mortality") %>% ungroup()
-
-tidy_km <- km %>% 
-  broom::tidy() %>% 
-  transmute(
-    time = time, 
-    strata = ifelse(strata == "smoke_dic=0", "Never smoked", "Ever smoked"),
-    cif = 1 - estimate,
-    conf.low2 = 1- conf.low,
-    conf.high2 = 1 - conf.high) %>% 
-  rename(conf.high = conf.low2,
-         conf.low = conf.high2) %>% 
-  mutate(model = "B. Direct effect on dementia risk") 
-
-output <- bind_rows(tidy_cif, tidy_km, tidy_death)
-
-return(output)
+  
+  tidy_km <- km %>%
+    broom::tidy() %>%
+    transmute(
+      time = time,
+      strata = strata,
+      cif = 1 - estimate,
+      conf.low2 = 1 - conf.low,
+      conf.high2 = 1 - conf.high
+    ) %>%
+    rename(conf.high = conf.low2,
+           conf.low = conf.high2) %>%
+    mutate(
+      strata = ifelse(strata == "smoke_dic=0",
+                      "Non-smokers", "Smokers")) %>% 
+    mutate(model = "B. Direct effect on dementia risk")
+  
+  output <- bind_rows(tidy_cif, tidy_km, tidy_death)
+  
+  return(output)
 }
 
 
@@ -54,7 +71,17 @@ return(output)
 all_models_unadj <- bind_all_models(cif = dem_crude, death = death_crude, 
                               km_crude_conditional)
 
-all_models_unadj %>% 
+plot_unadj <- all_models_unadj %>% 
+  mutate(model = 
+    case_when(
+      model == "A. Total effect on dementia risk" ~ 
+        "A. Dementia risk \n without elimination of death",
+      model == "B. Direct effect on dementia risk" ~ 
+        "B. Dementia risk \n with elimination of death",
+      model == "C. Effect on mortality" ~ 
+        "C. Risk of death",
+    )
+  ) %>% 
   ggplot(aes(time, cif, group = strata)) +
   geom_line(aes(color = strata), size = 0.70) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high) ,alpha = 0.2) +
@@ -65,16 +92,30 @@ all_models_unadj %>%
     color = NULL,
     y = NULL,
     x = "Years of follow-up") +
-  theme_bw(base_size = 14, base_family = "serif") +
-  theme(legend.position = "bottom")
+  theme_bw(base_family = "serif") +
+  theme(legend.position = "bottom",
+        legend.text = element_text(size=12)) +
+  theme(strip.text.x = element_text(size = 11),
+        # strip.background = element_blank(),
+        strip.background = element_rect(fill=NA),
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=12))
 
+ggsave(filename = "plot_unadjusted.tiff", 
+       plot = plot_unadj, 
+       path = here::here("03_figs"),
+       device = "tiff",
+       width = 8,
+       dpi = "retina")
 
 # Plots after adjusting for confounding -----------------------------------
 
-all_models <- bind_all_models(cif = dem_adjusted, death = death_adjusted, 
-                              km = km_adjusted_conditional)
+all_models <-
+  bind_all_models(cif = dem_adjusted, 
+                  death = death_adjusted,
+                  km = km_adjusted_conditional)
 
-all_models %>% 
+plot_adjusted <- all_models %>% 
   ggplot(aes(time, cif, group = strata)) +
   geom_line(aes(color = strata), size = 0.7) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high) ,alpha = 0.2) +
@@ -85,6 +126,19 @@ all_models %>%
     color = NULL,
     y = NULL,
     x = "Years of follow-up") +
-  theme_bw(base_size = 14, base_family = "serif") +
-  theme(legend.position = "bottom")
+  theme_bw(base_family = "serif") +
+  theme(legend.position = "bottom",
+        legend.text = element_text(size=12)) +
+  theme(strip.text.x = element_text(size = 11),
+        # strip.background = element_blank(),
+        strip.background = element_rect(fill=NA),
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=12))
+
+ggsave(filename = "plot_adjusted.tiff", 
+       plot = plot_adjusted, 
+       path = here::here("03_figs"),
+       device = "tiff",
+       width = 8,
+       dpi = "retina")
 
